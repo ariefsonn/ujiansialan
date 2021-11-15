c5dce7b8847ddffc5c89abb9890f117189df3718
royyan1207070 <royyan120707@gmail.com>
2021-11-15 17:00:33 +0700

    bacot

--- app/src/main/java/com/example/ujiansialan/Activity/DetailStuff.java
+++ app/src/main/java/com/example/ujiansialan/Activity/DetailStuff.java
@@ -1,4 +1,31 @@
 package com.example.ujiansialan.Activity;
 
-public class DetailStuff {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ujiansialan.R;
+
+public class DetailStuff extends AppCompatActivity {
+
+    TextView tl, des, prc;
+    String tls, imgs, dess, prcs;
+    ImageView img;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_stuff);
+
+        tls = getIntent().getStringExtra("t_st");
+        prcs = getIntent().getStringExtra("pr_st");
+        imgs = getIntent().getStringExtra("img_st");
+
+
+
+    }
 }
